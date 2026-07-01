ed5561d221be3b28eb7a38efd90adc0a8f7b1c30
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2026-07-01 15:29:20 +0300

    feat: add runtime assembly entry point

---
+++ Runtime/Unity/Extensions/PersistenceExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Rula.Persistence.Unity.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for Unity integration.
+    /// </summary>
+    public static class PersistenceExtensions
+    {
+    }
+}
