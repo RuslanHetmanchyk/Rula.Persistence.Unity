e967a9d3966ff47587a344cb1a3d0395c4faa40a
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2026-07-01 17:51:47 +0300

    feat: add Unity save logger implementation

---
+++ Runtime/Unity/Logging/UnitySaveLogger.cs
@@ -0,0 +1,29 @@
+﻿using Rula.Persistence.Abstractions;
+using UnityEngine;
+
+namespace Rula.Persistence.Unity.Logging
+{
+    /// <summary>
+    /// Unity implementation of persistence logging.
+    /// </summary>
+    public sealed class UnitySaveLogger : ISaveLogger
+    {
+        /// <inheritdoc />
+        public void Log(string message)
+        {
+            Debug.Log(message);
+        }
+
+        /// <inheritdoc />
+        public void LogWarning(string message)
+        {
+            Debug.LogWarning(message);
+        }
+
+        /// <inheritdoc />
+        public void LogError(string message)
+        {
+            Debug.LogError(message);
+        }
+    }
+}
