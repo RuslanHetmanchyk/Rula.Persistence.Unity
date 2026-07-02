34d7d4342abf1734724645b1556b3897e6f25c62
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2026-07-02 12:07:05 +0300

    feat: add save directory detection

---
+++ Editor/Services/SaveFileService.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Rula.Persistence.Unity.Editor.Services
+{
+    internal static class SaveFileService
+    {
+        public static string SaveDirectory => Application.persistentDataPath;
+    }
+}
--- Editor/Windows/SaveEditorWindow.cs
+++ Editor/Windows/SaveEditorWindow.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using Rula.Persistence.Unity.Editor.Services;
 
 namespace Rula.Persistence.Unity.Editor.Windows
 {
@@ -14,9 +15,20 @@ namespace Rula.Persistence.Unity.Editor.Windows
         private void OnGUI()
         {
             GUILayout.Label("Rula Persistence Save Editor", EditorStyles.boldLabel);
+
             GUILayout.Space(10);
 
-            GUILayout.Label("Coming soon...");
+            EditorGUILayout.LabelField(
+                "Save Directory",
+                SaveFileService.SaveDirectory);
+
+            GUILayout.Space(10);
+
+            if (GUILayout.Button("Open Save Folder"))
+            {
+                UnityEditor.EditorUtility.RevealInFinder(
+                    SaveFileService.SaveDirectory);
+            }
         }
     }
 }
