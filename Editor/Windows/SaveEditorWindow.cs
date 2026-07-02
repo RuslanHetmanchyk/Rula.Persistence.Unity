a103616498827bb73503844e836000fe58862c77
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2026-07-02 11:53:54 +0300

    feat: add save editor window

---
+++ Editor/Windows/SaveEditorWindow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Rula.Persistence.Unity.Editor.Windows
+{
+    public sealed class SaveEditorWindow : EditorWindow
+    {
+        [MenuItem("Tools/Rula/Persistence/Save Editor")]
+        public static void Open()
+        {
+            GetWindow<SaveEditorWindow>("Save Editor");
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Rula Persistence Save Editor", EditorStyles.boldLabel);
+            GUILayout.Space(10);
+
+            GUILayout.Label("Coming soon...");
+        }
+    }
+}
